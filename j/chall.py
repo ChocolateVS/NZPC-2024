5e5734cd90eab58af070f0c542a696a44a6072d6
ChocolateVS <rghthorley@gmail.com>
2024-08-20 01:59:09 +1200

    solved j

---
+++ j/chall.py
@@ -0,0 +1,33 @@
+# Snaggle
+def evaluateExpression(exp: str):
+  try: 
+    if exp[0] == "(":                         # Checks if the expression has a bracket
+      exp = exp[1:len(exp):1]                 # Remove the bracket
+    else:
+      raise Exception                         # If the expression doesn't have a bracket its a integer
+  except:
+    numBrackets = exp.split(" ")[0].find(')') # Count the number of closing brackets in component of the expression 
+    if (numBrackets > -1):                    # If there are any, replace them all. 
+                                              # i.e. the expression: 200) -1000) would be split into ['200)', '-1000)']
+                                              # we would remove the bracket from '200)' > so that we can parse it and return the remaining expression
+      exp = exp.replace(')', '', numBrackets)
+    arr = exp.split(' ')
+    return [float(arr[0]), ' '.join(arr[1::])] # Parses the first index in the expression and returns the rest
+  
+  # Here means we are in an expression
+  [p, exp] = evaluateExpression(exp)      # Will parse p from the expression
+  [e1, exp] = evaluateExpression(exp)     # Will attempt to evaluate the expression at e1
+  [e2, _] = evaluateExpression(exp)       # Will attempt to evaluate the remaining part of the expression
+
+  remain = ' '.join(exp.split(" ")[1::])  # If we a solving a nested expression, the nested expression will be replaced with it's value
+                                          # The remaining expression will continue to be parsed
+  return [solveExpression(float(p), float(e1), float(e2)), remain]
+
+def solveExpression(p: float, x: float, y: float):
+  return ((p * (x + y)) + ((1 - p) * (x - y)))
+
+while (True):
+  expression = input()
+  if expression  == "()":
+    break
+  print(evaluateExpression(expression)[0])
