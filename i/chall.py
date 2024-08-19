41aeeca2a30fd34f98d4e86f066b03b3befdee9d
ChocolateVS <frankie@debian.org>
2024-08-19 19:30:01 +1200

    optimizations to challenge i

--- i/chall.py
+++ i/chall.py
@@ -1,3 +1,4 @@
+from typing import List
 [H, W] = [int(x) for x in input().split()]
 
 rows = []
@@ -10,49 +11,53 @@ memo = dict()
 sinks = dict()
 chars = dict()
 
-from typing import List
-
 def getLowest(cell: List[int]):
   lowest = cell
   x = cell[0]
   y = cell[1]
-  memory = memo.get(str(x) + str(y))
+  memory = memo.get(str(x) + " " + str(y))
   if (memory != None):
     return memory
 
   if (y - 1 >= 0 and rows[x][y] > rows[x][y - 1]):
     lowest = [x, y - 1]
+  
   if (x - 1 >= 0 and rows[x - 1][y] < rows[lowest[0]][lowest[1]] and rows[x][y] > rows[x - 1][y]):
     lowest = [x - 1, y]
+  
   if (x + 1 < H and rows[x + 1][y] < rows[lowest[0]][lowest[1]] and rows[x][y] > rows[x + 1][y]):
     lowest = [x + 1, y]
+  
   if (y + 1 < W and rows[x][y + 1] < rows[lowest[0]][lowest[1]] and rows[x][y] > rows[x][y + 1]):
     lowest = [x, y + 1]
 
-  
   if (x == lowest[0] and y == lowest[1]):
-    memo[str(x) + str(y)] = lowest
+    memo[str(x) + " " + str(y)] = lowest
     return lowest
 
   else:
-    return getLowest(lowest)
+    lowest = getLowest(lowest)
+    memo[str(x) + " " + str(y)] = lowest
+    return lowest
 
 
 for x in range(0, H):
   for y in range(0, W):
     lowest = getLowest([x, y])
-    cells[str(x) + str(y)] = str(lowest[0]) + str(lowest[1])
-char = 97
+    print('Check Point', memo)
+    cells[str(x) + " " + str(y)] = str(lowest[0]) + str(lowest[1])
+
+char = 'a'
 
 for c in cells.items():
   if (sinks.get(c[1]) == None):
-    sinks[c[1]] = chr(char)
-    char = char + 1
+    sinks[c[1]] = char
+    char = chr(ord(char) + 1)
 
   chars[c[0]] = sinks.get(c[1])
 
 for x in range(0, H):
   output = ""
   for y in range(0, W):
-    output = output + chars[str(x) + str(y)]
+    output = output + chars[str(x) + " " + str(y)]
   print(output)
